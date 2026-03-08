ddec4f2e8b85b029146c2b5b43d5aed2cf16f670
Rudolph Bester <Rudolph.f.bester@gmail.com>
2026-03-08 13:18:11 +0200

    Got  some stuff working again

--- servers/rendering/renderer_rd/shaders/effects/screen_space_shadows.glsl
+++ servers/rendering/renderer_rd/shaders/effects/screen_space_shadows.glsl
@@ -6,7 +6,7 @@
 
 #include "../light_data_inc.glsl"
 
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+layout(local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
 
 layout(set = 0, binding = 0) uniform sampler2D source_hiz;
 layout(set = 0, binding = 1, std140) uniform SceneData {
@@ -24,13 +24,12 @@ layout(rgba16f, set = 0, binding = 4) uniform restrict writeonly image2D output_
 
 layout(push_constant, std430) uniform Params {
 	ivec2 screen_size;
-	int mipmaps;
-	int num_steps;
+	ivec2 light_offset;
 	vec4 light_coordinate;
-	float distance_fade;
-	float curve_fade_in;
+	float min;
+	float max;
 	float depth_tolerance;
-	bool orthogonal;
+	int max_steps;
 	int view_index;
 	int debug_enabled;
 	int debug_mode;
@@ -38,6 +37,11 @@ layout(push_constant, std430) uniform Params {
 params;
 
 #define M_PI 3.14159265359
+#define WAVE_SIZE 64
+#define SAMPLE_COUNT 60
+#define READ_COUNT (SAMPLE_COUNT / WAVE_SIZE + 2) // 2
+
+shared float DepthData[READ_COUNT * WAVE_SIZE];
 
 float linearize_depth(float depth) {
 	vec4 pos = vec4(0.0, 0.0, depth, 1.0);
@@ -46,34 +50,105 @@ float linearize_depth(float depth) {
 }
 
 void main() {
-	ivec2 pixel_pos = ivec2(gl_GlobalInvocationID.xy);
+	ivec2 group_offset = ivec2(gl_WorkGroupID.yz);
+	int group_id = int(gl_WorkGroupID.x);
+	int thread_id = int(gl_LocalInvocationID.x);
+	vec4 light = params.light_coordinate;
+
+	vec2 light_xy = floor(light.xy) + 0.5;
+	vec2 light_xy_fraction = light.xy - light_xy;
+	bool reverse_direction = light.w < 0.0f;
+
+	ivec2 grid_offset = group_offset * WAVE_SIZE + params.light_offset;
+	vec2 group_dir;
+	float axis_start = 0.0;
+	bool x_major_axis = false;
+
+	if (grid_offset.y <= grid_offset.x && grid_offset.x > -grid_offset.y) {
+		// East of light -> major axis direction down
+		group_dir = vec2(0.0, -1.0);
+		axis_start = abs(grid_offset.x) + light_xy_fraction.x;
+		x_major_axis = false;
+	} else if (grid_offset.x <= -grid_offset.y && grid_offset.x > grid_offset.y) {
+		// South of light -> major axis direction left
+		group_dir = vec2(-1.0, 0.0);
+		axis_start = abs(grid_offset.y) + light_xy_fraction.y;
+		x_major_axis = true;
+	} else if (grid_offset.x <= grid_offset.y && -grid_offset.x > grid_offset.y) {
+		// West of light -> major axis direction up
+		group_dir = vec2(0.0, 1.0);
+		axis_start = abs(grid_offset.x) + light_xy_fraction.x;
+		x_major_axis = false;
+	} else if (-grid_offset.x <= grid_offset.y && grid_offset.x < grid_offset.y) {
+		// North of light -> major axis direction right
+		group_dir = vec2(1.0, 0.0);
+		axis_start = abs(grid_offset.y) + light_xy_fraction.y;
+		x_major_axis = true;
+	} else {
+		return;
+	}
+
+	vec2 group_start = grid_offset + light_xy + group_dir * group_id;
+	vec2 group_end = mix(light.xy, group_start, max((axis_start - WAVE_SIZE), 0.0) / axis_start);
+
+	// swap
+	if (reverse_direction) {
+		vec2 temp = group_start;
+		group_start = group_end;
+		group_end = temp;
+	}
+
+	// Flip direction if light rays are diverging
+	vec2 group_delta = group_end - group_start;
+	vec2 pixel_pos = mix(group_start, group_end, float(thread_id) / WAVE_SIZE);
+
+	float pixel_distance = !reverse_direction ? axis_start - thread_id : axis_start + thread_id;
 
 	if (any(greaterThanEqual(pixel_pos, params.screen_size))) {
 		return;
 	}
 
-	vec4 light = params.light_coordinate;
+	const float direction = reverse_direction ? 1.0 : -1.0;
+
+	float shadowing_depth[READ_COUNT];
+	float sample_distance[READ_COUNT];
+	float depth_thickness_scale[READ_COUNT];
+
+	vec2 xy_offset = vec2(0, 0);
+	for (int i = 0; i < READ_COUNT; i++) {
+		shadowing_depth[i] = texelFetch(source_hiz, ivec2(pixel_pos + xy_offset), 0).x;
+		sample_distance[i] = pixel_distance - (WAVE_SIZE * i) * direction;
+
+		int idx = (i * WAVE_SIZE) + thread_id;
+		DepthData[idx] = linearize_depth(shadowing_depth[i]);
+		xy_offset += group_delta;
+	}
+
+	memoryBarrierShared();
+	barrier();
 
 	vec3 screen_pos;
 	screen_pos.xy = pixel_pos;
-	screen_pos.z = texelFetch(source_hiz, pixel_pos, 0).x; // TEST Linearize??
+	screen_pos.z = shadowing_depth[0];
 
-	vec3 screen_dir = normalize(light.xyz - screen_pos.xyz) * light.w;
+	vec3 screen_dir = light.w * normalize(light.xyz - screen_pos.xyz);
 
-	vec2 t0 = (vec2(0.0) - screen_pos.xy) / screen_dir.xy;
-	vec2 t1 = (vec2(1.0) - screen_pos.xy) / screen_dir.xy;
-	vec2 t2 = max(t0, t1);
-	float t_max = min(t2.x, t2.y);
+	if (x_major_axis) {
+		screen_dir = screen_dir / abs(screen_dir.y);
+	} else {
+		screen_dir = screen_dir / abs(screen_dir.x);
+	}
 
 	float ref_depth = screen_pos.z;
 	float shadow = 1.0;
 
-	for (int i = 1; i < 64; i++) {
+	for (int i = 1; i < 40; i++) {
 		ivec2 xy_offset = ivec2(screen_dir.xy * i);
 		float depth_offset = screen_dir.z * i;
 
 		float expected_depth = linearize_depth(ref_depth + depth_offset);
-		float measured_depth = linearize_depth(texelFetch(source_hiz, pixel_pos + xy_offset, 0).x);
+		//		float measured_depth = linearize_depth(texelFetch(source_hiz, ivec2(pixel_pos + xy_offset), 0).x);
+		float measured_depth = DepthData[thread_id + i];
 
 		float diff = measured_depth - expected_depth;
 
@@ -84,62 +159,49 @@ void main() {
 		}
 	}
 
-	vec4 depth_color = vec4(screen_pos.z, screen_pos.z, screen_pos.z, 0.0);
-	imageStore(output_shadow, pixel_pos, vec4(shadow, 0.0, 0.0, 0.0));
-	imageStore(output_mip_level, pixel_pos, depth_color);
+	ivec2 ipixel_pos = ivec2(pixel_pos);
+	imageStore(output_shadow, ipixel_pos, vec4(shadow, 0.0, 0.0, 0.0));
+	//	imageStore(output_mip_level, ipixel_pos, depth_color);
 
 	vec4 line_color = vec4(0.0, 0.0, 0.0, 0.0);
 	switch (params.debug_mode) {
-		case 2: {
-			ivec2 i = params.screen_size.xy / 2;
-			vec2 p = pixel_pos - i;
+		case 5:
 
-			vec2 d = normalize(light.xy - i) * light.w;
-			//				d.y = -d.y;
-			bool p_sign = (sign(p.x) == sign(d.x) && sign(p.y) == sign(d.y));
-			if (floor(p.x * d.y) == floor(p.y * d.x) && p_sign) {
-				line_color.g = 1.0;
-				line_color.a = 1.0;
+			float dista = clamp(abs(1) / 100000.0, 0.0, 1.0);
+			imageStore(output_debug, ipixel_pos, vec4(dista, dista, dista, 1.0));
+			break;
+
+		case 4:
+
+			float group = gl_WorkGroupID.x / 63.0;
+			imageStore(output_debug, ipixel_pos, vec4(group, group, group, 1.0));
+			break;
+		case 3:
+			imageStore(output_debug, ipixel_pos, vec4(shadow, shadow, shadow, shadow));
+			break;
+		case 2:
+			// Individual
+			float test_depth2 = abs(linearize_depth(texelFetch(source_hiz, ivec2(pixel_pos + screen_dir.xy * params.max_steps), 0).x));
+			test_depth2 -= abs(DepthData[thread_id + params.max_steps]);
+			test_depth2 = abs(test_depth2);
+			if (test_depth2 < 0.001) {
+				test_depth2 = 1.0;
 			}
-		}
-
-			{
-				ivec2 i = ivec2(500, 400);
-				vec2 p = pixel_pos - i;
-
-				vec2 d = normalize(light.xy - i) * light.w;
-
-				bool p_sign = (sign(p.x) == sign(d.x) && sign(p.y) == sign(d.y));
-				if (floor(p.x * d.y) == floor(p.y * d.x) && p_sign) {
-					line_color.g = 1.0;
-					line_color.a = 1.0;
-				}
-			}
-
-			{
-				ivec2 i = ivec2(800, 400);
-				vec2 p = pixel_pos - i;
-				vec2 d = normalize(light.xy - i) * light.w;
-
-				bool p_sign = (sign(p.x) == sign(d.x) && sign(p.y) == sign(d.y));
-				if (floor(p.x * d.y) == floor(p.y * d.x) && p_sign) {
-					line_color.g = 1.0;
-					line_color.a = 1.0;
-				}
-			}
-
-			if (ivec2(light.xy) == pixel_pos) {
-				line_color.b = 1.0;
-				line_color.a = 1.0;
-			}
-			imageStore(output_debug, pixel_pos, line_color);
+			//			test_depth2 = min(test_depth2, abs(linearize_depth(ref_depth) / 200.0));
+			vec4 depth_debug2 = vec4(test_depth2, test_depth2, test_depth2, 1.0);
+			imageStore(output_debug, ipixel_pos, depth_debug2);
 			break;
 		case 1:
-			imageStore(output_debug, pixel_pos, vec4(shadow, shadow, shadow, shadow));
+			// Shared
+			float test_depth1 = abs(linearize_depth(texelFetch(source_hiz, ivec2(pixel_pos + screen_dir.xy * params.max_steps), 0).x)) / 200.0;
+			vec4 depth_debug1 = vec4(test_depth1, test_depth1, test_depth1, 1.0);
+			imageStore(output_debug, ipixel_pos, depth_debug1);
 			break;
 		case 0:
 		default:
-			imageStore(output_debug, pixel_pos, depth_color);
+			float test_depth = abs(DepthData[thread_id + params.max_steps]) / 200.0;
+			vec4 depth_debug = vec4(test_depth, test_depth, test_depth, 1.0);
+			imageStore(output_debug, ipixel_pos, depth_debug);
 			break;
 	}
 }
